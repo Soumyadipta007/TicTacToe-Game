c192f097b5f7e93cad8eb9a8b4fb7ec3d5f10438
Soumya <soumyadipta0077@gmail.com>
2020-10-13 09:52:04 +0530

    [Soumya] Add: Creating Board

---
+++ TicTacToe_Game/TicTacToe_Game/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicTacToe_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] board = createBoard();
+        }
+        private static char[] createBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
