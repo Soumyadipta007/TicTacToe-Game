a11c576673b1fd5feedae41387b532ba70e13cc4
Soumya <soumyadipta0077@gmail.com>
2020-10-13 10:48:57 +0530

    [Soumya] Refactor: Taking input X or 0 for player and computer

---
+++ TicTacToe_Game/TicTacToe_Game/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe_Game
+{
+    class Player
+    {
+        string name;
+        char symbol;
+        public Player(string name,char symbol)
+        {
+            this.name = name;
+            this.symbol = symbol;
+        }
+    }
+}
--- TicTacToe_Game/TicTacToe_Game/Program.cs
+++ TicTacToe_Game/TicTacToe_Game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.SymbolStore;
 
 namespace TicTacToe_Game
 {
@@ -7,6 +8,14 @@ namespace TicTacToe_Game
         static void Main(string[] args)
         {
             char[] board = createBoard();
+            char symbolForPlayer = chooseXor0("Player");
+            Player player = new Player("Player", symbolForPlayer);
+            char symbolForComputer;
+            if (symbolForPlayer == 'X')
+                symbolForComputer = '0';
+            else
+                symbolForComputer = 'X';
+            Player computer = new Player("Computer", symbolForComputer);
         }
         private static char[] createBoard()
         {
@@ -16,6 +25,18 @@ namespace TicTacToe_Game
                 board[i] = ' ';
             }
             return board;
+        }
+        private static char chooseXor0(string name)
+        {
+            char symbol;
+            while (true)
+            {
+                Console.WriteLine("Enter Symbol X or 0 for "+name);
+                symbol = Console.ReadLine()[0];
+                if (symbol == 'X' || symbol == '0')
+                    break;
+            }
+            return symbol;
         }
     }
 }
